12cadb257e9ce25d1ab3eef025325926fbf84a6a
Gopikannan-fssa <118425968+Gopikannan-fssa@users.noreply.github.com>
2023-08-24 17:42:21 +0530

    added doc comments

--- src/main/java/com/fssa/pin/service/exception/ServiceException.java
+++ src/main/java/com/fssa/pin/service/exception/ServiceException.java
@@ -1,5 +1,8 @@
 package com.fssa.pin.service.exception;
 
+/**
+ * User defined exception class for service-related errors.
+ */
 public class ServiceException extends Exception {
 
 	/**
@@ -8,19 +11,32 @@ public class ServiceException extends Exception {
 	private static final long serialVersionUID = -2166346220002004434L;
 
 	/**
-	 * 
+	 * Creates a new ServiceException with the given error message.
+	 *
+	 * @param msg The error message describing the reason for the exception.
 	 */
-	
 	public ServiceException(String msg) {
 		super(msg);
 
 	}
-	
+
+	/**
+	 * Creates a new ServiceException with the given underlying cause.
+	 *
+	 * @param e The underlying cause of the exception.
+	 */
 	public ServiceException(Throwable e) {
 		super(e);
 
 	}
-	
+
+	/**
+	 * Creates a new ServiceException with the given error message and underlying
+	 * cause.
+	 *
+	 * @param msg The error message describing the reason for the exception.
+	 * @param e   The underlying cause of the exception.
+	 */
 	public ServiceException(String string, Throwable e) {
 		super(e);
 
